28e45698c4cf60d924effbc85a8adc7877b0fc37
HemanthTenneti <hemanth10etii@gmail.com>
2025-03-28 14:51:09 +0530

    TESTING: Adding backend API routes
    Used cors.
    Wrote basic backend routes for /api/login and /api/signup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(cors());
 
-export default app
+app.post("/api/login", async (context) => {
+  const body = await context.req.json();
+  return context.json({ message: "Login", data: body });
+});
+
+app.post("/api/signup", async (context) => {
+  const body = await context.req.json();
+  return context.json({ message: "Signup", data: body });
+});
+
+export default {
+  port: 5269,
+  fetch: app.fetch,
+};
